8167ea175ca691934ce1abfb97a0279947a235be
kzrhodes <kzrhodes@dundee.ac.uk>
2014-02-20 21:53:14 +0000

    updated documentation

--- search.py
+++ search.py
@@ -1,15 +1,19 @@
+# -*- coding: iso-8859-1 -*-
+#imports models script
 import models
 
 Gene = models.Gene(%s)
 
+#imports cgi programmes
 import cgi
 import cgitb
 cgitb.enable()
 
 form = cgi.FieldStorage()
+#displays the data ouput to the website
 
-print "Content-Type: text/html"
-print
+print "Content-Type: text/html" #html stuff is following
+print #blank line, end of headers
 print "<html><head><TITLE>Results</TITLE></head>"
 print "<body><H1>Results</H1>"
 print "<table><tr><th>Ke</th><th>Value</th></tr>"

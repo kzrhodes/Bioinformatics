df9711e362f6f8a9aa94997c94ea9a48f30b71a8
kzrhodes <kzrhodes@dundee.ac.uk>
2014-02-20 21:52:05 +0000

    updated documentation

--- models.py
+++ models.py
@@ -3,6 +3,7 @@
 
 import MySQLdb
 
+#class allows script to login to personal MySQL with created tables
 class DBHandler():
     connection=None
     dbname='kzrhodes'
@@ -17,6 +18,7 @@ user=DBHandler.dbuser, passwd=DBHandler.dbpassword)
     def cursor(self):
 	return DBHandler.connection.cursor()
 
+#class which keeps the data created from search function
 class Gene():
     gene_symbol=''
     gene_title=''
@@ -25,21 +27,25 @@ class Gene():
 
     def __init__(self,geneid):
 	self.gene_id=geneid
+
+	#logs into MySQL
         db=DBHandler()
 	cursor=db.cursor()
 	sql='select gene_title, gene_symbol from gene where gene_id=%s'
 	cursor.execute(sql,(geneid,))
+
 	#query database
 	#get result and populate the class fields.
 	result=cursor.fetchone()
 
 	self.gene_title	=result[0]
         self.gene_symbol=result[1]
-        #now fetch the probes..
+        #locates matching probes
         probesql='select probe_name from probe where gene=%s'
 	#fill in the blanks yourself
 	cursor.execute(probesql,(geneid,))
 
+	#fills probe list
 	for result in cursor.fetchall():
   	    self.probelist.append(result[0])
 
